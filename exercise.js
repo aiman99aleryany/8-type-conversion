55db2c741737efe60480f7a117eda82a9c784fbc
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 04:02:20 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,19 @@
+// # Exercise 8
+
+// * Create a variable and set it to `true`.
+// * Print the value in the console and the data type.
+// * Convert it to a numeric data type, print the value and the data type.
+// * Convert it to string data type, print the value and the data type.
+// * Finally, convert it to a boolean data type, print the value and the data type.
+
+let boolVar = true;
+console.log(boolVar, typeof boolVar);
+
+boolVar = Number(boolVar);
+console.log(boolVar, typeof boolVar);
+
+boolVar = String(boolVar);
+console.log(boolVar, typeof boolVar);
+
+boolVar = Boolean(boolVar);
+console.log(boolVar, typeof boolVar);
